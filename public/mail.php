d9a140790e3a55e8c83c3018a32a240176567191
Michal_M <michalmaruna2157421574@gmail.com>
2026-02-02 18:41:17 +0100

    New OG + urls

--- public/mail.php
+++ public/mail.php
@@ -1,8 +1,6 @@
 <?php
-// mail.php – jednoduchý PHP handler pre odosielanie e-mailov z formulára
-
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: https://www.targos.sk'); // uprav podľa domény
+header('Access-Control-Allow-Origin: https://www.targos.sk');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
@@ -11,7 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-// Získaj dáta z POST
 $data = json_decode(file_get_contents('php://input'), true);
 $name = trim($data['name'] ?? '');
 $email = trim($data['email'] ?? '');
@@ -25,6 +22,7 @@ if (!$name || !$email || !$interest) {
     exit();
 }
 
+
 $year = date('Y');
 $replace = [
     '{{name}}' => $name,
@@ -35,8 +33,17 @@ $replace = [
     '{{year}}' => $year
 ];
 
+// Rozlíšenie typu formulára podľa interest (ak obsahuje "kontakt")
+$isContact = (stripos($interest, 'kontakt') !== false);
+
 // Admin šablóna (HTML alebo TXT)
-if (file_exists('admin-notification.html')) {
+if ($isContact && file_exists('admin-notification-contact.html')) {
+    $adminTpl = file_get_contents('admin-notification-contact.html');
+    $adminBody = str_replace(array_keys($replace), array_values($replace), $adminTpl);
+} else if ($isContact && file_exists('admin-notification-contact.txt')) {
+    $adminTpl = file_get_contents('admin-notification-contact.txt');
+    $adminBody = str_replace(array_keys($replace), array_values($replace), $adminTpl);
+} else if (file_exists('admin-notification.html')) {
     $adminTpl = file_get_contents('admin-notification.html');
     $adminBody = str_replace(array_keys($replace), array_values($replace), $adminTpl);
 } else if (file_exists('admin-notification.txt')) {
@@ -47,7 +54,13 @@ if (file_exists('admin-notification.html')) {
 }
 
 // Klient šablóna (HTML alebo TXT)
-if (file_exists('client-confirmation.html')) {
+if ($isContact && file_exists('client-confirmation-contact.html')) {
+    $clientTpl = file_get_contents('client-confirmation-contact.html');
+    $clientBody = str_replace(array_keys($replace), array_values($replace), $clientTpl);
+} else if ($isContact && file_exists('client-confirmation-contact.txt')) {
+    $clientTpl = file_get_contents('client-confirmation-contact.txt');
+    $clientBody = str_replace(array_keys($replace), array_values($replace), $clientTpl);
+} else if (file_exists('client-confirmation.html')) {
     $clientTpl = file_get_contents('client-confirmation.html');
     $clientBody = str_replace(array_keys($replace), array_values($replace), $clientTpl);
 } else if (file_exists('client-confirmation.txt')) {
@@ -57,11 +70,13 @@ if (file_exists('client-confirmation.html')) {
     $clientBody = "Ďakujeme za váš dopyt, $name!\nVaša požiadavka bola prijatá.\n\nKópia vašej správy:\n$message";
 }
 
-$adminMail = 'info@targos.sk'; // uprav na svoj e-mail
+$adminMail = 'info@targos.sk';
 $from = "info@targos.sk";
 
-// Jednoduché hlavičky
-$headers = "From: $from";
+// HTML hlavičky
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8\r\n";
+$headers .= "From: $from\r\n";
 
 // Pošli adminovi
 $sentAdmin = mail($adminMail, "Nový dopyt z webu: $interest", $adminBody, $headers);
