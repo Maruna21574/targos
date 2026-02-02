3e0ffee2a6afe0eed404747e398ae4e28aa64163
Michal_M <michalmaruna2157421574@gmail.com>
2026-02-02 17:52:24 +0100

    New OG + urls

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -27,7 +27,7 @@ const Contact: React.FC<ContactProps> = ({ prefilledMessage = '', setPrefilledMe
     e.preventDefault();
     setIsSubmitting(true);
     try {
-      const response = await fetch('https://www.targos.sk/mail.php', {
+      const response = await fetch('https://api.targos.sk/mail.php', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
